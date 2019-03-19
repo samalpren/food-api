cab8ab16d90f8c9276f002e6c7f77d3caf3456c3
Sam Alpren <samalpren1@gmail.com>
2019-03-19 09:50:10 -0500

    Finally figured out how to pull ingredients and information from the worldfoods api

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -8,6 +8,11 @@ const stringTemplate = (input) => {
             <h1>${input.name}</h1>
             <p>${input.category}</p>
             <p>${input.ethnicity}</p>
+            <p>${input.ingredients}</p>
+            <p>${input.countries}</p>
+            <p>${input.energy}</p>
+            <p>${input.sugars_value}</p>
+            <p>${input.fat_value}</p>
         </div>
     `;
 }
@@ -20,7 +25,22 @@ fetch("http://localhost:8088/food")
     .then(food => food.json())
     .then(parsedFood => {
         parsedFood.forEach(food => {
-            render(food)
+            
+            console.log(food);
+
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                .then(response => response.json())
+                .then(productInfo => {
+                     food.ingredients = `Ingredients: ${productInfo.product.ingredients_text}`
+                     food.countries = `Country: ${productInfo.product.countries}`
+                     food.energy = `Calories ${productInfo.product.nutriments.energy}` 
+                     food.sugars_value = `Sugar: ${productInfo.product.nutriments.sugars_value}`
+                     food.fat_value = `Fat: ${productInfo.product.nutriments.fat_value}`
+ 
+                     // Add representaiton to DOM
+                     console.log(food)
+                     foodEntries.innerHTML += stringTemplate(food)
+                })
         })
     });
 
